23e714d3d78ceff17996f8d2c5e9dd19a2ec3cb4
vlkvsky <vlkvsky@gmail.com>
2016-08-02 21:23:53 +0300

    import TableConcstructor.java

---
+++ src/model/TableConstructor.java
@@ -0,0 +1,50 @@
+package src.model;
+
+import org.nocrala.tools.texttablefmt.BorderStyle;
+import org.nocrala.tools.texttablefmt.ShownBorders;
+import org.nocrala.tools.texttablefmt.Table;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class TableConstructor {
+
+    private Table table;
+    private Set<String> columns;
+    private List<Map<String, Object>> tableData;
+
+    public TableConstructor(Set<String> columns, List<Map<String, Object>> tableData) {
+        this.columns = columns;
+        this.tableData = tableData;
+        table = new Table(columns.size(), BorderStyle.CLASSIC, ShownBorders.ALL);
+    }
+
+    public String getTableString() {
+        build();
+        return table.render();
+    }
+
+    private void build() {
+        buildHeader();
+        buildRows();
+    }
+
+    private void buildHeader() {
+        for (String column : columns) {
+            table.addCell(column);
+        }
+    }
+
+    private void buildRows() {
+        for (Map<String, Object> row : tableData) {
+            for (Object value : row.values()) {
+                if (value != null) {
+                    table.addCell(value.toString());
+                } else {
+                    table.addCell("");
+                }
+            }
+        }
+    }
+}
